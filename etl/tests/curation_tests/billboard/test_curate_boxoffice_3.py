63f7826f32bf31fa09d070f4f4fac2ef7e371f9b
Matt Quigley <mquigs00@gmail.com>
2026-01-08 06:45:58 -0500

    Updated location curation to match existing locations despite typos

--- etl/tests/curation_tests/billboard/test_curate_boxoffice_3.py
+++ etl/tests/curation_tests/billboard/test_curate_boxoffice_3.py
@@ -1,8 +1,5 @@
-import pandas as pd
 from etl.curation.magazines.billboard import curate_boxoffice_3 as curator
 from datetime import date
-
-from etl.curation.magazines.billboard.curate_boxoffice_3 import parse_artist_names
 from utils.utils import *
 import pytest
 
@@ -11,6 +8,16 @@ dim_venues = load_dimension_tables()["venues"]
 dim_cities = load_dimension_tables()["cities"]
 
 # DATES
+def test_clean_dates_text():
+    dates = ['Multt-Purpose']
+    clean_dates = curator.clean_dates(dates)
+    assert clean_dates == ''
+
+def test_clean_dates_numbers():
+    dates = ['Sept. 28', 'Sept. 30 9144.zia 10,894']
+    clean_dates = curator.clean_dates(dates)
+    assert clean_dates == 'Sept. 28 Sept. 30'
+
 def test_curate_noise_in_dates():
     dates = ['Sept. 30 9144.zia 10,894']
     issue_year = 1984
@@ -20,13 +27,12 @@ def test_curate_noise_in_dates():
     assert end_date == date(1984, 9, 30)
 
 # ARTISTS
-@pytest.mark.only
 def test_curate_artists_festival():
     # if there is an event name, artists should be split by commas, and ampersands at the end of lines should signal to join adjacent artists
     orig_artists = ['BUDWEISER SUPERFEST:', 'PEABO BRYSON, KOOL &', 'THE GANG, WHISPERS,', 'MTUME, PATTI LABELLE']
     event_name, artists = curator.parse_event_name(orig_artists)
     has_event_name = event_name is not None
-    artists = parse_artist_names(artists, has_event_name)
+    artists = curator.parse_artist_names(artists, has_event_name)
     assert event_name == 'Budweiser Superfest'
     assert artists == ['PEABO BRYSON', 'KOOL & THE GANG', 'WHISPERS', 'MTUME', 'PATTI LABELLE']
 
@@ -35,7 +41,7 @@ def test_curate_artists_with_comma():
     artists = ['CROSBY, STILLS & NASH']
     event_name, artists = curator.parse_event_name(artists)
     has_event_name = event_name is not None
-    artists = parse_artist_names(artists, has_event_name)
+    artists = curator.parse_artist_names(artists, has_event_name)
     assert event_name is None
     assert artists == ['CROSBY, STILLS & NASH']
 
@@ -66,6 +72,39 @@ def test_match_state_in_venue_ambiguous():
     state_id = curator.match_state_in_venue(location_data)
     assert state_id == -1
 
+def test_city_at_end_of_venue_name():
+    location_tokens = ['Univ.', 'of', 'Colorado', 'at', 'Boulder']
+    city_id, city_name, city_index = curator.match_city_after_venue(location_tokens, None, dim_cities)
+    if city_id is not None:
+        location_tokens = location_tokens[:city_index]
+    location_data = curator.clean_location(location_tokens)
+    venue_name = " ".join(location_data)
+    assert city_name == 'Boulder'
+    assert venue_name == 'Univ. of Colorado at Boulder'
+
+@pytest.mark.only
+def test_match_city_in_venue_typo():
+    location_tokens = ['Harttord', '(Conn.)', 'Civic', 'Center', '&', 'Associates', 'for', 'Pertorming', 'Arts']
+    state_id, location_tokens = curator.match_state_in_venue(location_tokens)
+    city_candidate, venue_type_idx = curator.find_city_candidate(location_tokens)  # find what looks like a city name
+    if venue_type_idx:                                                                                                  # remove city candidate from location tokens
+        location_tokens = location_tokens[:venue_type_idx + 1]
+    print(location_tokens)
+    city_id = curator.match_city_in_venue(location_tokens, dim_cities, state_id)
+    location_tokens = curator.clean_venue_name(location_tokens)
+    venue_name = " ".join(location_tokens)
+    venue_id, venue_name = curator.match_existing_venues(venue_name, dim_venues, city_id, city_candidate, dim_cities)
+    assert state_id == 7
+    assert city_id == 132
+    assert venue_name == 'Hartford Civic Center'
+
+def test_venue_keyword_before_end_of_name():
+    # venue name should end at the last event keyword, not at the first event keyword
+    location_data = ['Memorial', 'Coliseum']
+    location_data = curator.clean_venue_name(location_data)
+    venue_name = " ".join(location_data)
+    assert venue_name == "Memorial Coliseum"
+
 def test_match_city_after_venue():
     # although Dallas is in the location, it should not match it because there is no state provided
     test_data = ['Reunion', 'Arena', 'Dallas', 'Productions']
@@ -86,6 +125,25 @@ def test_match_city_in_venue_noise():
     state_id = curator.match_state_after_venue(test_data)
     city_id, city_name, city_index = curator.match_city_in_venue(test_data, dim_cities, state_id)
     assert city_name == 'San Francisco'
+
+def test_find_city_state_after_venue():
+    test_data = ['Hilton', 'Coliseum', 'Ames', 'Iowa']
+    state_id = curator.match_state_after_venue(test_data)
+    city_id, city_name, city_index = curator.match_city_after_venue(test_data, state_id, dim_cities)
+    assert state_id == 15
+    assert city_name == 'Ames'
+
+def test_match_city_after_venue_state_in_venue_name():
+    test_data = ['Middle', 'Tennessee', 'State', 'Univ.', 'Murphy Center', 'Murfreesboro']
+    state_id = curator.match_state_after_venue(test_data)
+    city_id, city_name, city_index = curator.match_city_after_venue(test_data, state_id, dim_cities)
+    assert state_id is None
+    assert city_id is None
+
+def test_find_state_in_venue_no_parentheses():
+    test_data = ['Rochester' 'N.Y,' 'War', 'Memorial']
+    state_id = curator.match_state_in_venue(test_data)
+    assert state_id == 32
 
 def test_find_venue_type_idx():
     test_data = ['Frank', 'Cainer', 'Arena', 'Behan', 'Park', 'Nanaima']
--- utils/utils.py
+++ utils/utils.py
@@ -125,6 +125,10 @@ def get_venue_name(venue_id):
     if isinstance(venue_id, str):
         if venue_id.isdigit():
             venue_id = int(venue_id)
+    elif isinstance(venue_id, bool):
+        raise TypeError("venue_id must be int, not bool")
+    if not isinstance(venue_id, int):
+        print(f"venue_id must be int, not {type(venue_id)}")
 
     dimension_tables = load_dimension_tables()
     dim_venues_by_id = dimension_tables["venues"]["by_id"]
@@ -185,3 +189,8 @@ def clean_artist_name(artist):
     artist = artist.strip()
     artist = re.sub(r"[._:|{}\[\]]", "", artist)
     return artist
+
+def get_source_id(source_slug):
+    dim_sources = load_dimension_tables()["sources"]["by_slug"]
+    source_id = dim_sources[source_slug][0]["id"]
+    return source_id
