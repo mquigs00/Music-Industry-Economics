e1b7e37868e57b55e053875fe41229de37d1193c
Matt Quigley <mquigs00@gmail.com>
2026-01-28 11:38:44 -0500

    Remove obsolete parser files and add lambda files

--- etl/main.py
+++ etl/main.py
@@ -1,14 +1,10 @@
-from config.paths import DIM_CITIES_PATH
-from etl.parsers.magazines.billboard import extract_boxoffice_1
-from etl.parsers.magazines.billboard import extract_boxoffice_3
-from etl.curation.magazines.billboard import curate_boxoffice_3 as curator
-from config import paths
-from utils import utils
-from data_cleaning import clean_csv_for_glue
-import re
-import os
-from datetime import date
+from etl.schemas.billboard_magazine_3.curation import curate as curator
+from etl.schemas.billboard_magazine_3.curation.artists import parse_artist_names
+from etl.schemas.billboard_magazine_3.curation.special_event import calc_special_event_score
+from etl.schemas.billboard_magazine_3.processing.process import extract_to_csv
+from utils.utils import *
+import slugify
 
 if __name__ == "__main__":
-    #extract_boxoffice_3.extract_to_csv()
+    #extract_to_csv()
     curator.curate_events()
